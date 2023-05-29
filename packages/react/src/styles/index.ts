e4ffb549adee947a7e1acaacfee6d03ec4f44745
Pablo Alan <pabloxt14@gmail.com>
2023-05-29 16:24:54 -0300

    feat: configuring stitches on react package

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,39 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  // a opção "themeMap" é usada para mapear valores de um tema para uma propriedade CSS específica
+  themeMap: {
+    ...defaultThemeMap, // mapeamento padrão do stitches
+    // especificando que as propriedades width e height devem herdar os valores do token de space
+    width: 'space',
+    height: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
